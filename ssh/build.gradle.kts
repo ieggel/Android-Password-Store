fa4b8f35827508e2c01c88ec8318ba385a3941f9
Aditya Wasan <adityawasan55@gmail.com>
2022-01-25 21:00:18 +0530

    chore(ssh): add required dependencies
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- ssh/build.gradle.kts
+++ ssh/build.gradle.kts
@@ -7,6 +7,11 @@ plugins {
 android { sourceSets { getByName("test") { resources.srcDir("src/main/res/raw") } } }
 
 dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.kotlin.coroutines.android)
+  implementation(libs.kotlin.coroutines.core)
   implementation(libs.thirdparty.sshauth)
   implementation(libs.thirdparty.sshj)
+  implementation(libs.androidx.security)
+  implementation(libs.thirdparty.eddsa)
 }
